285b9e702473726aed303383ed97bc24ff17955e
goptun <ramos.matheus@icloud.com>
2023-08-06 13:48:34 -0300

    [M1S09] Ex 4 - Criação adicionar tarefas

--- lista-de-tarefas/src/App.jsx
+++ lista-de-tarefas/src/App.jsx
@@ -5,9 +5,19 @@ import CardAdicionar from "./components/CardAdicionar";
 function App() {
   const [listaTarefas, setListaTarefas] = useState([]);
 
+  const adicionarTarefa = (texto) => {
+    if (texto == "") {
+      alert("Digite uma tarefa");
+      return;
+    }
+
+    const novaTarefa = { id: listaTarefas.length + 1, textoTarefa: texto };
+    setListaTarefas([...listaTarefas, novaTarefa]);
+  };
+
   return (
     <>
-      <CardAdicionar />
+      <CardAdicionar adicionarTarefa={adicionarTarefa} />
     </>
   );
 }
